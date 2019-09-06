b3aeb22a3ddd08753037a7f6c44f705c5c4d0574
KoukiFactori <asridev51@gmail.com>
2019-09-06 18:41:38 +0200

    move index.js into src directory

---
+++ src/index.js
@@ -0,0 +1,58 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+const axios = require('axios')
+const fs = require('fs')
+
+const { prompt } = require('inquirer')
+const chalk = require('chalk') 
+
+prompt([
+	{ name: "token", message: "Client token :"}
+]).then(({ token }) => {
+	client.login(token).catch(() => { console.error(chalk`{red /!\\ }{bold Invalid token !}`), process.exit() })
+})
+
+
+client.on('ready', async () => {
+	const guilds = client.guilds.map(({ id, name }) => `${id} - ${name}`)
+	getGuild(guilds)
+		.then(({ guild: guildID }) => downloadEmojis(guildID))
+})
+
+const getGuild = (guilds) => {
+	return prompt([
+		{
+			name: "guild",
+			type: 'list',
+			message: 'Select a guild :',
+			filter: function(str) {
+				return str.split(' ')[0]
+			},
+			choices: guilds
+		}
+	])
+}
+
+const downloadEmojis = async (guildID) => {
+	const emojis = client.guilds.get(guildID).emojis.map(({  name, id, animated }) => [name, id, animated])
+	for await (const emoji of emojis) {
+		const url = `https://cdn.discordapp.com/emojis/${emoji[1]}.${emoji[2] ? "gif" : "png"}`
+
+		const file = fs.createWriteStream(`${emoji[0]}.${emoji[2] ? "gif" : "png"}`)
+		
+		await axios({url, responseType: 'stream'})
+			.then(({ data }) => data.pipe(file))
+	}
+	
+	await prompt({
+		type: 'confirm',
+		name: 'restart',
+		message: 'Download finished, do you want to download emotes from another server?'
+	}).then(({ restart }) => {
+		if(!restart) { console.error(chalk`{bold Shutdown program...}`), process.exit() }
+		client.emit('ready')
+	})
+	
+}
+		
